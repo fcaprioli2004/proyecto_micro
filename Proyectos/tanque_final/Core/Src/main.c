e180ce8d0bee6f7be9d67b2d7f7f2af0d1ccc9b0
fcaprioli2004 <fcaprioli2004@gmail.com>
2026-08-19 10:36:52 -0300

    termino pruebas

--- Proyectos/tanque_final/Core/Src/main.c
+++ Proyectos/tanque_final/Core/Src/main.c
@@ -17,7 +17,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h" //hola fausto hola peter chau fausto
+#include "main.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
