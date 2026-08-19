37569c0d19381c3f667c9f574a616c3dc73da64d
peterortiz03 <peter.ortiz03@gmail.com>
2026-08-19 10:26:57 -0300

    cambio en tanque

--- Proyectos/tanque_final/Core/Src/main.c
+++ Proyectos/tanque_final/Core/Src/main.c
@@ -17,7 +17,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "main.h" //hola fausto
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
